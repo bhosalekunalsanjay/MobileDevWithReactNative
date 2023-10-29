ab7bca1440f3a8ba456d570d9106b311acac4a0c
kunal sanjay bhosale <Kunal.Bhosale.Admin@savills.com.hk>
2023-10-29 20:59:20 +0530

    adding firebase

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Text, useColorScheme, View } from 'react-native';
 import { dummyApi } from './utils/ApiService';
+// import { onValue, ref } from 'firebase/database';
+// import { db } from './Firebase.Config';
+// import firestore from '@react-native-firebase/firestore';
+// import { getDoc } from 'firebase/firestore';
+// import { collection, doc, setDoc } from "firebase/firestore";
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+//   const citiesRef = ()=>{
+//     var a = collection(db, "cities");
+//     console.log(a);
+//   }
+
+  // getDoc();
+  // const collection = doc(FIREBASE_DATABASE,'collectionname','docname');
+  // const dbCollection = collection(FIREBASEAPP.firestore(), collectionName);
+  // const a = firestore.collection('as');
 
   const dummyApiCall = () => {
     dummyApi().then(response => {
@@ -15,6 +29,19 @@ function App(): JSX.Element {
         // Handle errors here
       });
   }
+
+//   useEffect(() => {
+//       const d = firestore().collection('user').onSnapshot(q => {
+//         const u = []
+//         q.forEach(doc=>{
+//           u.push({
+//             ...doc.data(),
+//             key:doc.id
+//           })
+//         })
+//       })
+//     return () => citiesRef();
+//   }, []);
 
   return (
     <View>
